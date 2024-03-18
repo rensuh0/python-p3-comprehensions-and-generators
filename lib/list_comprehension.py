39639cd9e73942a59fa4e92f8bde4057a7dd4d02
Najeeb <najhassanali@gmail.com>
2024-03-18 15:12:39 +0300

    Assignment done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    if not evens:
+        return []
+    else:
+        return evens
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return []
+    else:
+        return [sentence + '!' for sentence in sentence_list]
